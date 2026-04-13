fae5e296f02431f33d27ca21849fc350092b03b0
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2026-04-13 10:56:33 -0500

    13/04/2026

---
+++ modulo 1/basics/16_ciclo_for.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Controles de fuñlo interaciones, ciclos repetitivos, ciclo while")
+    println("While basico")
+    
+    var contador =1
+    while(contador <=5){
+        println(contador)
+        contador++
+    }
+    
+}
