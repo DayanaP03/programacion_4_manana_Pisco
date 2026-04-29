d47cf0ef707490ff60d5d1c96f29a9218e6d95fd
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2026-04-29 11:08:06 -0500

    29/04/2026

--- modulo 2/tts/app/src/main/java/com/ute/tts/MainActivityCalculator.kt
+++ modulo 2/tts/app/src/main/java/com/ute/tts/MainActivityCalculator.kt
@@ -17,7 +17,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivityCalculator : AppCompatActivity() {
 
 
     private lateinit var webView: WebView
