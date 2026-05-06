eab7c13dc78f29929e11cd2aa7aaa5aa214b7700
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2026-05-06 10:59:38 -0500

    06/05/2026

--- modulo 2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
+++ modulo 2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.ute.compose.ui.material3.Paso01SumaScreen
+import com.ute.compose.ui.material3.Paso01TextFieldScreen
 
 import com.ute.compose.ui.screens.*
 
@@ -13,9 +15,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                // ◀ CAMBIA AQUÍ para probar cada paso:
+                // ◀ CAMBIA AQUÍ para probar cada paso
+                // //Componentes bascicos nativos:
                 //S01_SaludoScreen()
-                S02_TextScreen()
+                //S02_TextScreen()
+               // S03ButtonScreen()
+                //S04LayoutScreen()
+                //S05ModifierScreen()
+                //S06EstadoScreen()
+                //S07StateHoistingScreen()
+                //S08BienvenidaScreen()
+
+                //componente material 3
+                // ◀ CAMBIA AQUÍ para probar cada paso:
+                //Paso01TextFieldScreen()
+                Paso01SumaScreen()
+                // Paso02_CardScreen()
+                // Paso03_LazyColumnScreen()
+                // Paso04_ScaffoldScreen()
+                // Paso05_NavBarScreen()
+
             // reen()
                 // Paso03_LazyColumnScreen()
                 // Paso04_ScaffoldScreen()
