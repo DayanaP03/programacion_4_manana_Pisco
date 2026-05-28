c11369ed03e172838fb197cb845747b9b530e787
DayanaP03 <josselynpisco1755@gmail.com>
2026-05-28 05:03:33 -0500

    login-register

--- modulo 2/ShopApplication/app/src/main/java/com/shopapp/domain/repository/CategoryRepository.kt
+++ modulo 2/ShopApplication/app/src/main/java/com/shopapp/domain/repository/CategoryRepository.kt
@@ -1,10 +1,8 @@
+// domain/repository/CategoryRepository.kt
 package com.shopapp.domain.repository
 
 import com.shopapp.domain.model.Category
 import com.shopapp.domain.model.CategoryPayload
-import com.shopapp.domain.model.Product
-import com.shopapp.domain.model.ProductFilters
-import com.shopapp.domain.model.ProductPayload
 
 interface CategoryRepository {
     suspend fun getCategories(): Result<List<Category>>
@@ -14,12 +12,4 @@ interface CategoryRepository {
     suspend fun deleteCategory(id: Int): Result<Unit>
 }
 
-interface ProductRepository {
-    suspend fun getProducts(filters: ProductFilters): Result<Pair<List<Product>, Int>>
-    suspend fun getProduct(id: Int): Result<Product>
-    suspend fun createProduct(payload: ProductPayload): Result<Product>
-    suspend fun updateProduct(id: Int, payload: ProductPayload): Result<Product>
-    suspend fun deleteProduct(id: Int): Result<Unit>
-    suspend fun restock(id: Int, quantity: Int): Result<Int>
-    suspend fun getStats(): Result<Map<String, Any>>
-}
+
